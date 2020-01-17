dc38280693c8db73a71d024088e0c47833ed2b9c
Christian Sweet <csweet@utexas.edu>
2020-01-17 17:54:40 -0600

    More update testing.

--- update_repo.py
+++ update_repo.py
@@ -3,7 +3,8 @@ import subprocess
 def main():
 	upstream_repo = "https://github.com/fri-robotlearning/Homework-0"
 
-	subprocess.Popen(["git", "add", "remote", "upstream", upstream_repo], stdout=subprocess.PIPE)
+	subprocess.Popen(["git", "remote", "add", "upstream", upstream_repo], stdout=subprocess.PIPE)
+	subprocess.Popen(["git", "fetch", "upstream"], stdout=subprocess.PIPE)
 	subprocess.Popen(["git", "pull", "upstream", "master"], stdout=subprocess.PIPE)
 	
 if __name__ == "__main__":
